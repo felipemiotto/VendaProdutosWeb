7e9f69d3cda252896d02fe6a6a00b4584eb87926
luizfernandorodrigues <luiz@jmti.com.br>
2018-10-27 17:44:34 -0200

    Adicionado layout de vendas e itens da venda

--- src/main/java/web/JsfItensVenda.java
+++ src/main/java/web/JsfItensVenda.java
@@ -5,6 +5,9 @@
  */
 package web;
 
+import Model.Produto;
+import java.math.BigDecimal;
+import java.util.UUID;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
 
@@ -16,6 +19,60 @@ import javax.enterprise.context.Dependent;
 @Dependent
 public class JsfItensVenda {
 
+    private UUID id;
+    private BigDecimal quantidade;
+    private BigDecimal valorUnitario;
+    private BigDecimal desconto;
+    private UUID vendaId;
+    private Produto produto;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public BigDecimal getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(BigDecimal quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(BigDecimal valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    public BigDecimal getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(BigDecimal desconto) {
+        this.desconto = desconto;
+    }
+
+    public UUID getVendaId() {
+        return vendaId;
+    }
+
+    public void setVendaId(UUID vendaId) {
+        this.vendaId = vendaId;
+    }
+
+    public Produto getProdutoId() {
+        return produto;
+    }
+
+    public void setProdutoId(Produto produto) {
+        this.produto = produto;
+    }
     /**
      * Creates a new instance of JsfItensVenda
      */
